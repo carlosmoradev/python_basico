6cf671b1831f06cf013ecc0e2368694f70e6fd07
Carlos Mora <carlos.mora@devimed.com.co>
2021-06-23 10:32:36 -0500

    first commit

---
+++ condicionales.py
@@ -0,0 +1,8 @@
+numero = int(input('Escribe un numero: '))
+
+if numero > 5:
+    print('El numero es mayor a 5')
+elif numero == 5:
+    print('El numero es igual a 5')
+else:
+    print('el numero es menor que 5')
---
+++ peso_dolar.py
@@ -0,0 +1,4 @@
+pesos = float(input("cuantos pesos Colombianos tienes? "))
+valor_dolar = 3875
+dolares = pesos / valor_dolar
+print("Tienes $" + str(round(dolares, 2)) + " dolares.")
