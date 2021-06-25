012ea2ad72902c17ab931b96a32d86b6d5ce92aa
Carlos Mora <carlos.mora@devimed.com.co>
2021-06-25 16:26:58 -0500

    :tada: juego basico finalizado

--- adivina_numero.py
+++ adivina_numero.py
@@ -5,7 +5,11 @@ def run():
     numero_aleatorio = random.randint(1, 100)
     numero_elegido = int(input('Eligen un numero entre el 1 y el 100: '))
     while numero_elegido != numero_aleatorio:
-        pass
+        if numero_elegido < numero_aleatorio:
+            print('Busca un numero mas grande')
+        else:
+            print('Busca un numero mas pequeño')
+            numero_elegido = int(input('Elige otro numero: '))
     print('Ganaste!!!')
 
 
