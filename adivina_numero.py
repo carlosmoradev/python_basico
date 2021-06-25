bb2b00bf14ae74c9c181aa08d75c1da369e6eea5
Carlos Mora <carlos.mora@devimed.com.co>
2021-06-25 16:23:48 -0500

    prueba a doble repositorio

--- adivina_numero.py
+++ adivina_numero.py
@@ -2,7 +2,11 @@ import random
 
 
 def run():
-    pass
+    numero_aleatorio = random.randint(1, 100)
+    numero_elegido = int(input('Eligen un numero entre el 1 y el 100: '))
+    while numero_elegido != numero_aleatorio:
+        pass
+    print('Ganaste!!!')
 
 
 if __name__ == '__main__':
